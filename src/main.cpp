f983dc019f7354b15e4b531923aa771b8ae24339
Jo <joseph.gedeon@uconn.edu>
2025-09-25 14:07:02 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,9 @@ int main()
     int a = 3;
     int b = 4;
 
-    cout << add(x, y) << endl;
-    cout << multiply(x, y) << endl;
-    cout << square(x) << endl;
+cout << "a + b = " << add(a, b) << endl;
+cout << "a * b = " << multiply(a, b) << endl;
+cout << "square(a) = " << square(a) << endl;
 
     return 0;
 }
